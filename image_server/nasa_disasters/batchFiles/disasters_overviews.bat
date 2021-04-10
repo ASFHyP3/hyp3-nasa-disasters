a8a0c5c1bb650b520a5174392259da45b3fbd606
Heidi Kristenson <hjkristenson@alaska.edu>
2021-04-09 19:02:07 -0800

    update path names to match disasters_mosaics.bat file

--- image_server/nasa_disasters/batchFiles/disasters_overviews.bat
+++ image_server/nasa_disasters/batchFiles/disasters_overviews.bat
@@ -1,7 +1,7 @@
 set ppath="C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" 
 set mdcspath=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\nasa_disasters
-set gdbwks=C:\Users\ASF\Documents\COVID19\Disasters\Esri\MosaicDatasets\missouri_disasters_mosaic.gdb
-set outcrf=C:\Users\ASF\Documents\COVID19\Disasters\FloodAreas\NASA_Disasters_AWS.acs\esri\missouri_disasters_mosaic.crf
+set gdbwks=C:\Users\ASF\Documents\COVID19\Disasters\Esri\MosaicDatasets\missouri_disasters_mosaics.gdb
+set outcrf=C:\Users\ASF\Documents\COVID19\Disasters\FloodAreas\NASA_Disasters_AWS.acs\esri\missouri_disasters_mosaics_sar_comp.crf
 
 REM ---------- Using ACS File -------------------------
 
