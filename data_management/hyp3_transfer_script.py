067fd7ce7fb473c4781ca248efd6535e14b37ea6
Andrew Johnston <asjohnston@alaska.edu>
2021-04-27 13:28:06 -0800

    refactoring

--- data_management/hyp3_transfer_script.py
+++ data_management/hyp3_transfer_script.py
@@ -8,13 +8,6 @@ from botocore.exceptions import ClientError
 S3 = boto3.resource('s3')
 
 
-def get_args():
-    parser = ArgumentParser()
-    parser.add_argument('--target-bucket', default='hyp3-nasa-disasters')
-    parser.add_argument('project_name')
-    return parser.parse_args()
-
-
 def object_exists(bucket, key):
     try:
         S3.Object(bucket, key).load()
@@ -24,6 +17,7 @@ def object_exists(bucket, key):
 
 
 def copy_object(source_bucket, source_key, target_bucket, target_key, chunk_size=104857600):
+    print(f'copying {source_bucket + "/" + source_key} to {target_bucket + "/" + target_key}')
     bucket = S3.Bucket(target_bucket)
     copy_source = {'Bucket': source_bucket, 'Key': source_key}
     transfer_config = TransferConfig(multipart_threshold=chunk_size, multipart_chunksize=chunk_size)
@@ -31,24 +25,35 @@ def copy_object(source_bucket, source_key, target_bucket, target_key, chunk_size
 
 
 def main():
-    args = get_args()
-
     hyp3 = hyp3_sdk.HyP3(prompt=True)
-    jobs = hyp3.find_jobs(name=args.project_name)
-    print(jobs)
-    print(f'Found {len(jobs)} jobs to copy')
+    project_name = input('HyP3 project name: ')
+    target_bucket = input('Destination bucket: ')
 
+    jobs = hyp3.find_jobs(name=project_name)
+    print(f'\nProject {project_name}: {jobs}')
+
+    print('\nLooking for new files to copy...')
+
+    objects_to_copy = []
     for job in jobs:
         source_bucket = job.files[0]['s3']['bucket']
         zip_key = job.files[0]['s3']['key']
         for ext in ('_VV.tif', '_VH.tif', '_rgb.tif', '_VV.tif.xml'):
             source_key = zip_key.replace('.zip', ext)
             target_key = source_key.replace(job.job_id, job.name)
-            if object_exists(args.target_bucket, target_key):
-                print(f'{args.target_bucket + "/" + target_key} already exists, skipping')
-            else:
-                print(f'copying {source_bucket + "/" + source_key} to {args.target_bucket + "/" + target_key}')
-                copy_object(source_bucket, source_key, args.target_bucket, target_key)
+            if not object_exists(target_bucket, target_key):
+                objects_to_copy.append({
+                    'source_bucket': source_bucket,
+                    'source_key': source_key,
+                    'target_bucket': target_bucket,
+                    'target_key': target_key,
+                })
+
+    print(f'\nFound {len(objects_to_copy)} new files to copy to s3://{target_bucket}/{project_name}/')
+    input('Press Enter to continue, Ctrl-c to cancel')
+
+    for object_to_copy in objects_to_copy:
+        copy_object(**object_to_copy)
 
 
 if __name__ == '__main__':
