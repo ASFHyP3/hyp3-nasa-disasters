82c5d2e1016dba3fd6602070981a2968f43c86bf
Heidi Kristenson <hjkristenson@alaska.edu>
2021-04-27 17:57:40 -0800

    add batch file for updating existing MDs

---
+++ image_server/nasa_disasters/batchFiles/disasters_mosaics_update.bat
@@ -0,0 +1,12 @@
+set ppath="C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" 
+set mdcspath=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\nasa_disasters
+set cachepath=C:\Users\ASF\Documents\COVID19\Disasters\Esri\MosaicDatasets\PixelCache
+
+REM ---------- Set output gdb and source s3 bucket-----
+set gdbwks=C:\Users\ASF\Documents\COVID19\Disasters\Esri\MosaicDatasets\disasters_mosaic.gdb
+set acspath=C:\Users\ASF\Documents\COVID19\Disasters\FloodAreas\NASA_Disasters_AWS.acs\SET_S3_TAG_HERE\
+
+REM ---------- Using ACS File -------------------------
+%ppath% %mdcspath%\scripts\MDCS.py -i:%mdcspath%\Parameter\Config\disasters_mosaics_s1_update.xml -m:%gdbwks%\sar_s1 -s:%acspath% -p:%cachepath%\$cachelocation -p:USE_PIXEL_CACHE$pixelcache -c:CM+AF+AR+BF+BB+SP+CC+CV
+%ppath% %mdcspath%\scripts\MDCS.py -i:%mdcspath%\Parameter\Config\disasters_mosaics_comp_update.xml -m:%gdbwks%\sar_comp -s:%gdbwks%\sar_s1 -p:CompositeVV_VH_32.art.xml$art -c:CM+AR+UpdateNameField+BB+SP+CC
+
