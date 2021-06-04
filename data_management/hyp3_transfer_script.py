e4f32d8ff12ec688abee88eb4b524fee4ad84151
Joseph H Kennedy <me@jhkennedy.org>
2021-06-04 15:52:30 -0800

    Update transfer script for bangladesh

--- data_management/hyp3_transfer_script.py
+++ data_management/hyp3_transfer_script.py
@@ -1,3 +1,6 @@
+import json
+from argparse import ArgumentParser
+
 import boto3
 import hyp3_sdk
 from boto3.s3.transfer import TransferConfig
@@ -22,10 +25,19 @@ def copy_object(source_bucket, source_key, target_bucket, target_key, chunk_size
     bucket.copy(CopySource=copy_source, Key=target_key, Config=transfer_config)
 
 
-def main():
-    hyp3 = hyp3_sdk.HyP3(prompt=True)
-    project_name = input('HyP3 project name: ')
-    target_bucket = input('Destination bucket: ')
+def hyp3_transfer_script(config_file: str, prompt: bool = False):
+    with open(config_file) as f:
+        config = json.load(f)
+
+    hyp3 = hyp3_sdk.HyP3(config['host'], prompt=prompt)
+
+    project_name = config["project_name"]
+    target_bucket = config["transfer_spec"]["target_bucket"]
+    target_prefix = config["transfer_spec"].get("target_prefix", project_name)
+    if prompt:
+        project_name = input(f'HyP3 project name [{project_name}]: ') or project_name
+        target_bucket = input(f'Destination bucket: [{target_bucket}]') or target_bucket
+        target_prefix = input(f'Destination prefix: [{target_prefix}]') or target_prefix
 
     jobs = hyp3.find_jobs(name=project_name)
     print('\n' + project_name)
@@ -39,9 +51,9 @@ def main():
             continue
         source_bucket = job.files[0]['s3']['bucket']
         zip_key = job.files[0]['s3']['key']
-        for ext in ('_VV.tif', '_VH.tif', '_rgb.tif', '_VV.tif.xml'):
+        for ext in config["transfer_spec"]["extensions"]:
             source_key = zip_key.replace('.zip', ext)
-            target_key = source_key.replace(job.job_id, job.name)
+            target_key = source_key.replace(job.job_id, target_prefix)
             if not object_exists(target_bucket, target_key):
                 objects_to_copy.append({
                     'source_bucket': source_bucket,
@@ -50,7 +62,7 @@ def main():
                     'target_key': target_key,
                 })
 
-    print(f'\nFound {len(objects_to_copy)} new files to copy to s3://{target_bucket}/{project_name}/')
+    print(f'\nFound {len(objects_to_copy)} new files to copy to s3://{target_bucket}/{target_prefix}/')
     input('Press Enter to continue, Ctrl-c to cancel')
 
     for object_to_copy in objects_to_copy:
@@ -58,4 +70,11 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('config_file')
+    parser.add_argument('-y', '--yes', action='store_false',
+                        help='Provide a yes response to all prompts to always proceed')
+    args = parser.parse_args()
+
+    hyp3_transfer_script(args.cofig_file, prompt=args.yes)
+
