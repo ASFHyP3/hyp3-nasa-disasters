d66e88a22fecf866b84d3f866719bfc644f7d7d7
James Rine <jlrine2@alaska.edu>
2021-06-04 10:08:27 -0800

    initial work to automate bangeldesh watermap

--- data_management/process_new_granules.py
+++ data_management/process_new_granules.py
@@ -7,6 +7,7 @@ from hyp3_sdk import HyP3
 
 
 parser = ArgumentParser()
+parser.add_argument('-y', action='store_true')
 parser.add_argument('config_file')
 args = parser.parse_args()
 
@@ -15,13 +16,14 @@ with open(args.config_file) as f:
 
 print(f'Processing new granules for project {config["project_name"]}\n')
 
-hyp3 = HyP3(prompt=True)
+
+hyp3 = HyP3(config['host'], prompt=True) if not args.y else HyP3(config['host'])
 
 search_results = asf_search.geo_search(**config['search_parameters'])
 all_granules = {result.geojson()['properties']['sceneName'] for result in search_results}
 
 processed_jobs = hyp3.find_jobs(name=config['project_name'])
-processed_granules = {job.job_parameters['granules'][0] for job in processed_jobs.jobs}
+processed_granules = {job.job_parameters['granules'][0] for job in processed_jobs}
 
 granules_to_submit = all_granules - processed_granules
 print(f'\nFound {len(granules_to_submit)} new granules:')
@@ -30,14 +32,28 @@ for granule in granules_to_submit:
 
 print(f'\nFound {len(all_granules)} search results')
 print(f'Found {len(processed_granules)} granules already processed')
-print(f'\nSubmitting {len(granules_to_submit)} new RTC jobs to HyP3 with parameters:')
-pprint(config['job_parameters'])
-input('\nPress Enter to proceed, Ctrl-c to cancel')
+print(f'\nSubmitting {len(granules_to_submit)} new {config["job_spec"]["job_type"]} jobs to HyP3 with parameters:')
 
+pprint(config['job_spec']['job_parameters'])
+if not args.y:
+    input('\nPress Enter to proceed, Ctrl-c to cancel')
+
+
+jobs = []
 for granule in granules_to_submit:
-    batch = hyp3.submit_rtc_job(
-        granule=granule,
-        name=config['project_name'],
-        **config['job_parameters'],
-    )
-    print(f'Submitted RTC job for {granule}')
+    job_dict = config['job_spec'].copy()
+    job_dict['name'] = config['project_name']
+    job_dict['job_parameters']['granules'] = [granule]
+    jobs.append(job_dict)
+pprint(jobs)
+
+exit()
+def chunks(itr, n=200):
+    for i in range(0, len(itr), n):
+        yield itr[i:i + n]
+
+
+for sub_list in chunks(jobs):
+    batch = hyp3.submit_prepared_jobs(sub_list)
+
+print(f'submitted {len(jobs)} jobs to hyp3')
