87d1979f49a99c25f07d0743ac6fda7f5c7cfef3
Heidi Kristenson <hjkristenson@alaska.edu>
2021-07-08 17:11:05 -0800

    update schedule batch file to run both HKH and Hurricane processes

--- image_server/nasa_disasters_hkh/batchFiles/schedule.bat
+++ image_server/nasa_disasters_hkh/batchFiles/schedule.bat
@@ -1,1 +1,2 @@
 "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\nasa_disasters_hkh\batchFiles\generate_bats.py"
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\nasa_disasters_conus\batchFiles\update_hurricanes.py"
