45c2d7c69488e0490672e75f37f23a59c1431aa2
Heidi Kristenson <hjkristenson@alaska.edu>
2021-07-16 12:36:41 -0800

    add step in batch file to ensure portal credentials are in place

--- image_server/nasa_disasters_hkh/batchFiles/schedule.bat
+++ image_server/nasa_disasters_hkh/batchFiles/schedule.bat
@@ -1,2 +1,3 @@
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\portal_creds.py"
 "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\nasa_disasters_hkh\batchFiles\generate_bats.py"
 "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\nasa_disasters_conus\batchFiles\update_hurricanes.py"
