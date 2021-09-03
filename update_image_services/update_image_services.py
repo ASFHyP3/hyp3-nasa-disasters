8431248225d09312573dde13f607d6cc430ac552
Andrew Johnston <asjohnston@alaska.edu>
2021-09-03 12:09:36 -0800

    strip update_image_services.py down to the bare essentials

--- update_image_services/update_image_services.py
+++ update_image_services/update_image_services.py
@@ -1,154 +1,65 @@
+# arcpy.management.CreateCloudStorageConnectionFile('.', 'asj-hyp3-dev.acs', 'AMAZON', 'asj-hyp3-dev', region='us-west-2', config_options='AWS_NO_SIGN_REQUEST')
+
+import argparse
 import datetime
 import logging
 import os
 
 import arcpy
-import boto3
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
 arcpy.env.overwriteOutput = True
 
-# source_mds: full path to source mosaic dataset which will reference all images in the S3 bucket
-source_mds = r'C:\Users\rob10341\OneDrive - Esri\RPR_ESRI_Projects\NASA\NASA_scripting\NASA_scripting.gdb\image_management_testing_nasa_working_source'
 
-# derived_mds: full path to derived mosaic dataset which will hold the rasters from the source mosaic dataset after date
-# selection with overviews (crf)
-derived_mds = r'C:\Users\rob10341\OneDrive - Esri\RPR_ESRI_Projects\NASA\NASA_scripting\NASA_scripting.gdb\image_management_testing_nasa_working_derived'
-
-# reference_mds: full path to reference mosaic dataset which will be used for the image services (can be further
-# selected for date)
-reference_mds = r'C:\Users\rob10341\OneDrive - Esri\RPR_ESRI_Projects\NASA\NASA_scripting\NASA_scripting.gdb\image_management_testing_nasa_working_referenced'
-
-# acs_path: full path to the cloud connection file (.acs)
-acs_path = r'C:\Image_Mgmt_Workflows\NASA_hyp3-nasa-disasters.acs'
-
-# s3_bucket: S3 bucket name
-s3_bucket = 'hyp3-nasa-disasters'
-
-# s3 dir: directory structure after s3 bucket
-s3_dir = 'RTC_services'
-
-# image_type_filter: image type filter used to select the type of imagery to be added to the mosaic datasets. This is
-# necessary where different data sources are stored in the same bucket
-# For example, both rbg and VV + VH are stored in the same bucket. *rgb* | *VV* | *VH* as * is used as a wildcard
-image_type_filter = '*rgb*'
-
-# time_period_days: number of days from the current day to maintain in the derived mosaic dataset
-time_period_days = 31
-
-# ovi_time_period_days: number of days from the current day to delete overviews in S3
-ovi_time_period_days = 1
-
-
-class MosaicDataset:
-    def __init__(self, mosaic_dataset, raster_type, input_path):
-        self.mosaic_dataset = mosaic_dataset
-        self.raster_type = raster_type
-        self.input_path = input_path
-
-    def add_rasters_to_mosaic_dataset(self, **kwargs):
-        # https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/add-rasters-to-mosaic-dataset.htm
-        arcpy.management.AddRastersToMosaicDataset(in_mosaic_dataset=self.mosaic_dataset,
-                                                   raster_type=self.raster_type,
-                                                   input_path=self.input_path,
-                                                   **kwargs)
-        log.info(f'\n\nAdded raster files in {self.input_path} to {self.mosaic_dataset} mosaic dataset')
-
-    def build_boundary_mosaic_dataset(self, **kwargs):
-        # https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/build-boundary.htm
-        arcpy.management.BuildBoundary(in_mosaic_dataset=self.mosaic_dataset, **kwargs)
-        log.info(f'\n\nBuilt boundary for mosaic dataset {self.mosaic_dataset}')
-
-    def calculate_fields_with_selection(self, where_clause=None, **kwargs):
-        # https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/calculate-fields.htm
-        if where_clause:
-            selection = arcpy.management.SelectLayerByAttribute(self.mosaic_dataset, 'NEW_SELECTION', where_clause)
-            arcpy.management.CalculateFields(in_table=selection, **kwargs)
-        else:
-            arcpy.management.CalculateFields(in_table=self.mosaic_dataset, **kwargs)
-        log.info(f'\n\nCalculated field(s) for mosaic dataset: {self.mosaic_dataset}')
-
-    def remove_rasters_from_mosaic_dataset(self, **kwargs):
-        # https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/remove-rasters-from-mosaic-dataset.htm
-        arcpy.management.RemoveRastersFromMosaicDataset(in_mosaic_dataset=self.mosaic_dataset, **kwargs)
-        log.info(f'\n\nRemoved rasters from mosaic dataset {self.mosaic_dataset}')
-
-
-class Raster:
-    def __init__(self, in_raster, out_rasterdataset):
-        self.in_raster = in_raster
-        self.out_rasterdataset = out_rasterdataset
-
-    def copy_raster(self, **kwargs):
-        # https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/copy-raster.htm
-        arcpy.management.CopyRaster(in_raster=self.in_raster, out_rasterdataset=self.out_rasterdataset, **kwargs)
-        log.info(f'\n\nCopied raster {self.in_raster} to {self.out_rasterdataset}')
-
-
-class S3Object:
-    def __init__(self, s3_bucket, s3_dir, time_period_days,
-                 ovi_time_period_days):
-        self.s3_bucket = s3_bucket
-        self.s3_dir = s3_dir
-        self.time_period_days = time_period_days
-        self.ovi_time_period_days = ovi_time_period_days
-        self.s3 = boto3.client('s3')
-
-    def delete_s3_object_by_date(self):
-        raster_del_list = []
-        for obj in self.s3.list_objects(Bucket=self.s3_bucket, Prefix=self.s3_dir, Delimiter='')['Contents']:
-            s3_obj_key = obj['Key']
-            if ('tif' in s3_obj_key) and (
-                    datetime.datetime.today() - datetime.datetime.strptime(s3_obj_key.split('/')[-1].split('_')[2][0:8],
-                                                                           '%Y%m%d')).days >= self.time_period_days:
-                log.info(s3_obj_key)
-                raster_del_list.append(s3_obj_key)
-                # s3.delete_object(Bucket=s3_bucket, Key=s3_obj_key)
-            elif ('crf' in s3_obj_key) and ('_alllayers' in s3_obj_key) and (
-                    datetime.datetime.today() - datetime.datetime.strptime(
-                    s3_obj_key.split('/')[1].replace('.crf', '').replace('Ovi', '').replace('_', ''),
-                    '%Y%m%d')).days >= self.ovi_time_period_days:
-                log.info(s3_obj_key)
-                raster_del_list.append(s3_obj_key)
-                # s3.delete_object(Bucket=s3_bucket, Key=s3_obj_key)
-            elif ('crf' in s3_obj_key) and ('conf' in s3_obj_key) and (
-                    datetime.datetime.today() - datetime.datetime.strptime(
-                    s3_obj_key.split('/')[1].replace('.crf', '').replace('Ovi', '').replace('_', ''),
-                    '%Y%m%d')).days >= self.ovi_time_period_days:
-                log.info(s3_obj_key)
-                raster_del_list.append(s3_obj_key)
-                # s3.delete_object(Bucket=s3_bucket, Key=s3_obj_key)
-        return raster_del_list
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_mds',
+                        help='full path to source mosaic dataset which will reference all images in the S3 bucket')
+    parser.add_argument('derived_mds',
+                        help='full path to derived mosaic dataset which will hold the rasters from the source mosaic '
+                             'dataset after date')
+    parser.add_argument('reference_mds',
+                        help='full path to reference mosaic dataset which will be used for the image services (can be '
+                             'further selected for date)')
+    parser.add_argument('acs_path', help='full path to the cloud connection file (.acs)')
+    parser.add_argument('image_type_filter',
+                        help='image type filter used to select the type of imagery to be added to the mosaic datasets. '
+                             'This is necessary where different data sources  are stored in the same bucket For '
+                             'example, both rbg and VV + VH are stored in the same bucket. *rgb* | *VV* | *VH* as * is '
+                             'used as a wildcard')
+    parser.add_argument('s3_prefix', help='directory structure after s3 bucket')
+    parser.add_argument('--time-period-days', type=int, default=31,
+                        help='number of days from the current day to maintain in the derived mosaic dataset')
+    args = parser.parse_args()
+    return args
 
 
 def main():
+    args = get_args()
+
     # acs_path_s3: will update automatically to combine the acs_path and s3_bucket
-    acs_path_s3 = os.path.join(acs_path, s3_dir)
+    acs_path_s3 = os.path.join(args.acs_path, args.s3_prefix)
     # overview_crf: will update automatically to create a crf file through the acs named Ovi_
     overview_crf = os.path.join(acs_path_s3, 'Ovi_' + str(datetime.date.today()).replace('-', '_') + '.crf')
-    # sql_statement: sql statement to select rows with empty StartDate (newly added rasters)
-    sql_statement = 'StartDate IS NULL'
-    # date_sel: select a date that is less than or equal to the specified date range
-    date_sel = 'StartDate <= timestamp ' + '\'' + str(
-        (datetime.datetime.today() - datetime.timedelta(days=time_period_days)).strftime('%Y-%m-%d %I:%M:%S')) + "'"
-    # ovi_sel: select the overview files
-    ovi_sel = 'Name LIKE \'%Ovi%\''
 
-    log.info('\n\nStarting auto ingestion script')
-
-    # Add raster files and calculate the fields for the source mosaic dataset
-    manage_source = MosaicDataset(source_mds, 'Raster Dataset', acs_path_s3)
-    manage_source.add_rasters_to_mosaic_dataset(
-        update_cellsize_ranges='',
-        update_boundary='UPDATE_BOUNDARY',
-        update_overviews='NO_OVERVIEWS',
-        filter=image_type_filter,
+    log.info('Adding raster files and calculating the fields for the source mosaic dataset')
+    arcpy.management.AddRastersToMosaicDataset(
+        in_mosaic_dataset=args.source_mds,
+        raster_type='Raster Dataset',
+        input_path=acs_path_s3,
+        update_cellsize_ranges='NO_CELL_SIZES',
+        filter=args.image_type_filter,
         duplicate_items_action='EXCLUDE_DUPLICATES',
     )
-    manage_source.calculate_fields_with_selection(
-        where_clause=sql_statement,
+    selection = arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view=args.source_mds,
+        selection_type='NEW_SELECTION',
+        where_clause='StartDate IS NULL',
+    )
+    arcpy.management.CalculateFields(
+        in_table=selection,
         fields=[
             ['StartDate',
              '!Name!.split("_")[2][4:6] + "/" + !Name!.split("_")[2][6:8] + "/" + !Name!.split("_")[2][:4] + " " + '
@@ -161,46 +72,46 @@ def main():
         ],
     )
 
-    # Add new raster files and remove outdated raster files from the derived mosaic dataset
-    manage_derived = MosaicDataset(derived_mds, 'Table / Raster Catalog', source_mds)
-    manage_derived.add_rasters_to_mosaic_dataset(
-        update_cellsize_ranges='',
-        update_boundary='UPDATE_BOUNDARY',
-        update_overviews='NO_OVERVIEWS',
+    log.info('Adding new raster files and removing outdated raster files from the derived mosaic dataset')
+    arcpy.management.AddRastersToMosaicDataset(
+        in_mosaic_dataset=args.derived_mds,
+        raster_type='Table / Raster Catalog',
+        input_path=args.source_mds,
+        update_cellsize_ranges='NO_CELL_SIZES',
         duplicate_items_action='EXCLUDE_DUPLICATES',
     )
-    manage_derived.remove_rasters_from_mosaic_dataset(where_clause=date_sel, update_boundary='UPDATE_BOUNDARY')
+    cutoff_date = datetime.datetime.today() - datetime.timedelta(days=args.time_period_days)
+    date_sel = f"StartDate <= timestamp '{cutoff_date.strftime('%Y-%m-%d %I:%M:%S')}'"
+    arcpy.management.RemoveRastersFromMosaicDataset(in_mosaic_dataset=args.derived_mds, where_clause=date_sel)
 
-    # Create overview file for the derived mosaic dataset
-    manage_derived_create_ovi = Raster(derived_mds, overview_crf)
-    manage_derived_create_ovi.copy_raster()
+    log.info('Creating overview file for the derived mosaic dataset')
+    with arcpy.EnvManager(pyramid="PYRAMIDS 3"):
+        arcpy.management.CopyRaster(in_raster=args.derived_mds, out_rasterdataset=overview_crf)
 
-    # Add overview file to derived mosaic dataset and calculate fields
-    manage_derived = MosaicDataset(derived_mds, 'Raster Dataset', overview_crf)
-    manage_derived.add_rasters_to_mosaic_dataset(
-        update_cellsize_ranges='',
-        update_boundary='UPDATE_BOUNDARY',
-        update_overviews='NO_OVERVIEWS',
+    log.info('Adding overview file to derived mosaic dataset and calculating fields')
+    arcpy.management.AddRastersToMosaicDataset(
+        in_mosaic_dataset=args.derived_mds,
+        raster_type='Raster Dataset',
+        input_path=overview_crf,
+        update_cellsize_ranges='NO_CELL_SIZES',
     )
-
-    manage_derived.calculate_fields_with_selection(
-        where_clause=ovi_sel,
+    selection = arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view=args.derived_mds,
+        selection_type='NEW_SELECTION',
+        where_clause="Name LIKE '%Ovi%'",
+    )
+    arcpy.management.CalculateFields(
+        in_table=selection,
         fields=[
             ['MaxPS', '1610'],
             ['StartDate', '!Name!.split("_")[2] + "/" + !Name!.split("_")[3] + "/" + !Name!.split("_")[1]'],
         ],
     )
 
-    # Build the boundary file for the reference mosaic dataset
-    manage_reference = MosaicDataset(reference_mds, '', '')
-    manage_reference.build_boundary_mosaic_dataset()
+    log.info('Building the boundary file for the reference mosaic dataset')
+    arcpy.management.BuildBoundary(in_mosaic_dataset=args.reference_mds)
 
-    # Delete outdated raster files in S3
-    # del_s3 = S3Object(s3_bucket, s3_dir, time_period_days,
-    #                   ovi_time_period_days, log_messages.log)
-    # del_s3.delete_s3_object_by_date()
-
-    log.info('\n\nClosing auto ingestion script')
+    log.info('Finished')
 
 
 if __name__ == '__main__':
