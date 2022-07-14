3716a4284e0d966ef24047006d56f6a76e18a937
Heidi Kristenson <hjkristenson@alaska.edu>
2022-07-13 16:47:18 -0800

    delete vestigial directories from image_server directory

--- image_server/nasa_disasters_hkh/batchFiles/schedule.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\nasa_disasters_hkh\batchFiles\update_hkh.py"
