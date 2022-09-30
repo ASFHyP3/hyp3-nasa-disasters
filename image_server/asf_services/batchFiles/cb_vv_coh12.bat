1301f86586b9233df3b62bf2dc17eef7cc3f58e2
Heidi Kristenson <hjkristenson@alaska.edu>
2022-09-30 13:26:04 -0800

    add batch file for coherence dataset

---
+++ image_server/asf_services/batchFiles/cb_vv_coh12.bat
@@ -0,0 +1,11 @@
+set ppath="C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe"
+set mdcspath=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\asf_services
+set cachepath=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\asf_services\PixelCache
+
+REM ---------- Set output gdb and source s3 bucket-----
+set gdbwks=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\asf_services\MD\CandB\vv_coh12_220930C.gdb
+set outcrf=C:\Users\ASF\Documents\COVID19\Disasters\FloodAreas\NASA_Disasters_AWS.acs\esri\CandB_vv_coh12_220930C.crf
+
+REM ---------- Using ACS File -------------------------
+%ppath% %mdcspath%\scripts\MDCS.py -i:%mdcspath%\Parameter\Config\cb_vv_coh12.xml -m:%gdbwks%\vv_coh12 -p:%cachepath%\$cachelocation -p:USE_PIXEL_CACHE$pixelcache -c:CM+AF+AR+UpdateFieldsCoh+BF+BB+SP+CC
+%ppath% %mdcspath%\scripts\MDCS.py -i:%mdcspath%\Parameter\Config\cb_vv_coh12_ovr.xml -m:%gdbwks%\vv_coh12 -s:%outcrf% -c:SE+CRA+AR+UpdateCohOverviewFields -p:%outcrf%$outcrf
