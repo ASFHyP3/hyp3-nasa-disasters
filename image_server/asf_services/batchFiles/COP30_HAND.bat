0f6484fac612d837c33d9a52be914253381bd9db
hjkristenson <hjkristenson@alaska.edu>
2022-12-01 10:55:29 -0900

    updated HAND batch file

--- image_server/asf_services/batchFiles/COP30_HAND.bat
+++ image_server/asf_services/batchFiles/COP30_HAND.bat
@@ -3,10 +3,10 @@ set mdcspath=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_ser
 set cachepath=G:\Projects\2209_ImageServices\ImageServices\PixelCache
 
 REM ---------- Set output gdb and source s3 bucket-----
-set gdbwks=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\asf_services\MD\COP30_HAND\COP30_HAND_221129_2339.gdb
+set gdbwks=C:\Users\hjkristenson\PycharmProjects\hyp3-nasa-disasters\image_server\asf_services\MD\COP30_HAND\COP30_HAND_221130_2219.gdb
 set acspath=G:\Projects\2209_ImageServices\ImageServices\COP30_HAND.acs\
 set s3tag=v1/2021
-set outcrf=G:\Projects\2209_ImageServices\ImageServices\NASA_Disasters_AWS.acs\esri\COP30_HAND_221129_2339.crf
+set outcrf=G:\Projects\2209_ImageServices\ImageServices\NASA_Disasters_AWS.acs\esri\COP30_HAND_221130_2219.crf
 
 REM ---------- Using ACS File -------------------------
 %ppath% %mdcspath%\scripts\MDCS.py -i:%mdcspath%\Parameter\Config\\COP30_HAND.xml -m:%gdbwks%\\COP30_HAND -s:%acspath%%s3tag%\ -p:%cachepath%\$cachelocation -p:USE_PIXEL_CACHE$pixelcache -c:CM+AF+AR+UpdateFieldsHAND+BF+BB+SP+CC
